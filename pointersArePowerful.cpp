586fca20a3467f46a4f0b1d686ac68b2f5517547
Jimmy1826 <luis70202j@outlook.com>
2026-02-21 22:17:57 -0800

    Add files via upload

---
+++ pointersArePowerful.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+    int a = 10;
+    int* p;
+       p = &a;
+        std::cout << "Address stored in p: " << p << std::endl;
+    std::cout << "Value of a using p: " << *p << std::endl;
+    *p = 20;
+    std::cout << "New value of a: " << a << std::endl;
+
+    return 0;
+}
